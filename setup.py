c0f06675a2d2019e55587fe3b492f815d1937eba
gabpassos <47029367+gabpassos@users.noreply.github.com>
2020-09-26 09:51:49 -0300

    Finalização setup.py.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name= 'MatMovSolver',
+      version= '1.0.0',
+      description= 'Solver para o problema de alocação de alunos na ONG Matemática em Movimento',
+      long_description= long_description,
+      long_description_content_type= 'text/markdown',
+      author= 'Gabriel Passos, Flávia C. Gachet',
+      author_email= 'gabrielpassos97@hotmail.com',
+      url= 'https://github.com/gabpassos/matmov',
+      packages= ['MatMovSolver'],
+      license= 'MIT',
+
+      classifiers= [
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: MIT License',
+            'Environment :: Console',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.8',
+            'Operating System :: OS Independent'
+      ],
+
+      install_requires= [
+            'ortools >= 7.8.7959',
+            'pandas >= 1.1.2',
+            'numpy >= 1.19.2'
+      ],
+      python_requires= '>=3.7',
+)
